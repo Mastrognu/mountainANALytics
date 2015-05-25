e00517c8209c6d1677b4f603a54634fa89587bb1
mastrognu <mangusto.dev@gmail.com>
2015-05-25 16:09:40 +0200

    Commit2Merge

--- src/main/java/it/polimi/awt/services/FlickrService.java
+++ src/main/java/it/polimi/awt/services/FlickrService.java
@@ -1,8 +1,10 @@
 package it.polimi.awt.services;
 
+import java.util.Collection;
 import java.util.Collections;
 
 import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
 import com.flickr4java.flickr.photos.Photo;
 import com.flickr4java.flickr.photos.PhotoList;
@@ -11,13 +13,18 @@ import com.flickr4java.flickr.test.TestInterface;
 
 public class FlickrService {
 
-	String apiKey = "8ba1865ec3cc2789404826aaf1ec82c3";
-	String sharedSecret = "9794f74bdc128d35";
+	private static final String API_KEY = "8ba1865ec3cc2789404826aaf1ec82c3";
+	private static final String SHARED_SECRET = "9794f74bdc128d35";
 	
-	public void FlickrService() {
+	public FlickrService() throws FlickrException {
 		// Create a Flickr instance with your data. No need to authenticate
-		Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
-		
+		Flickr flickr = new Flickr(API_KEY, SHARED_SECRET, new REST());
+		TestInterface testInterface = flickr.getTestInterface();
+		Collection<?> coll = testInterface.echo(Collections.emptyMap());
+		for (Object element : coll) {
+			System.out.print(element.toString());
+		}
+
 		// Set the wanted search parameters
 	    SearchParameters searchParameters = new SearchParameters();
 //	    searchParameters.setAccuracy(accuracyLevel);
