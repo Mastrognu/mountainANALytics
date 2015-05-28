14780f61e5534390eb71bf0d404181fb65bbb9fe
SilvioValerio <SilvioValerio@192.168.1.108>
2015-05-28 22:49:56 +0200

    Inizio implementazione del parser json e inizio costruzione classe photo

--- src/main/java/it/polimi/awt/domain/Photo.java
+++ src/main/java/it/polimi/awt/domain/Photo.java
@@ -3,6 +3,24 @@ package it.polimi.awt.domain;
 
 public class Photo {
 	
+	private String user_id;
+	private String photo_id;
+
+	public String getUser_id(){
+		return user_id;
+	}
+	
+	public void setUser_id(String user_id){
+		this.user_id = user_id;
+	}
+	
+	public String getPhoto_id(){
+		return photo_id;
+	}
+	
+	public void setPhoto_id(String photo_id){
+		this.photo_id = photo_id;
+	}
 	
 
 }
--- src/main/java/it/polimi/awt/utils/JSONparser.java
+++ src/main/java/it/polimi/awt/utils/JSONparser.java
@@ -1,5 +1,22 @@
 package it.polimi.awt.utils;
 
+import java.io.IOException;
+
+import it.polimi.awt.domain.Photo;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//questo metodo prende un json per volta e lo traduce in un oggetto foto per volta
+//bisogna capire come ciclare su tutto lo stream e creare un botto di oggetti
+
 public class JSONparser {
+	
+	public static void mapper(String jsonObj) throws JsonParseException, JsonMappingException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper(); // can reuse, share globally
+		Photo photo = mapper.readValue(jsonObj, Photo.class);
+	}
 
 }
