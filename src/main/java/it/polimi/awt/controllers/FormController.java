36c17cbc2b544c60e88228ca00d8117e789cfae7
SilvioValerio <SilvioValerio@10.169.182.198>
2015-05-28 10:47:18 +0200

    Eliminato homo controller, adesso la prima jsp viene lanciata in automatico. Creato l'oggetto request per gestire la richiesta volta per volta.

--- src/main/java/it/polimi/awt/controllers/FormController.java
+++ src/main/java/it/polimi/awt/controllers/FormController.java
@@ -1,9 +1,17 @@
 package it.polimi.awt.controllers;
 
+import it.polimi.awt.domain.Request;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FormController {
+	
+	@RequestMapping("/mountainRQ")
+	public String addqueryFromForm(Request request) {		
+		return "jspdafare";
+	}
 	
 	
 
