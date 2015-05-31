3bb1333303703fceb504563045fe674bcb4817ce
mastrognu <mangusto.dev@gmail.com>
2015-05-31 17:18:46 +0200

    First UNTESTED implementation of the DB infrastructure done

---
+++ src/main/java/it/polimi/awt/repository/JpaMountainRepository.java
@@ -0,0 +1,29 @@
+package it.polimi.awt.repository;
+
+import it.polimi.awt.domain.Mountain;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JpaMountainRepository implements MountainRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public List<Mountain> findAll() {
+		TypedQuery<Mountain> query = em.createQuery("SELECT * FROM Mountain", Mountain.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public Mountain find(int mountainID) {
+		return em.find(Mountain.class, mountainID);
+	}
+}
