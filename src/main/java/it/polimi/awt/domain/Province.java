f1405a909268ea71fea509487701c03ce42aa6d7
mastrognu <mangusto.dev@gmail.com>
2015-07-07 14:44:08 +0200

    Nuove province non dannunziane

--- src/main/java/it/polimi/awt/domain/Province.java
+++ src/main/java/it/polimi/awt/domain/Province.java
@@ -12,6 +12,8 @@ public class Province {
 	@Id @Column(nullable = false) private String provinceID;
 	@Column(nullable = false) private String name;
 	@Column(nullable = false) private String region;
+	@Column(nullable = false) private double latitude;
+	@Column(nullable = false) private double longitude;
 
 	public Province() {
 	}
@@ -38,6 +40,22 @@ public class Province {
 
 	public void setRegion(String region) {
 		this.region = region;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
 	}
 
 	@Override
